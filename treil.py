65f0e6eab5434225da6e4ec6265b2af945e6bb57
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-16 01:27:46 +0700

    Update treil.py

--- treil.py
+++ treil.py
@@ -103,10 +103,9 @@ html_code = """
 
     <div class="footer">
         <p>dibuat oleh</p>
-        <p>Kelompok 13 Deep Learning</p>
-        <p>Program Studi Sains Data</p>
-        <p>Fakultas Sains</p> 
-        <p>Institut Teknologi Sumatera</p>
+        <h2>Kelompok 13 Deep Learning</h2>
+        <h3>Program Studi Sains Data, Fakultas Sains</h3> 
+        <h3>Institut Teknologi Sumatera</h3>
         <p>2024/2025</p>
     </div>
 
