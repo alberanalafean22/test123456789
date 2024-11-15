068840779fcd758ed05a591e96ebe0cc7aa30fb2
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-16 01:35:48 +0700

    Update treil.py

--- treil.py
+++ treil.py
@@ -110,6 +110,9 @@ html_code = """
         <h4>Rizki Adrian Bennovry, Alber Analafean, Nabilah Andika Fitriani, Catherine Firdhasari Maulina Sinaga, Helma Lia Putri</h4>
     </div>
 
+    <div>
+        <img class="logo" src="https://img.notionusercontent.com/s3/prod-files-secure%2F8cc0a41b-f9e2-4d9a-89b6-094a058ad494%2Fd594354d-d217-4ab5-b188-d31cc44df70f%2F13__1_-removebg-preview.png/freeze?exp=1731781811&sig=jCv2enD3Ca5vs4_NHzkhm36X4QnRGy2YtT0XHxWYhbg" alt="Logo ITE">
+    </div>
 
 
 </body>
