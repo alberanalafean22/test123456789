5f012f7f308b71d0da2c576ee048c03ce0491ccc
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-16 01:25:17 +0700

    Update treil.py

--- treil.py
+++ treil.py
@@ -117,5 +117,5 @@ html_code = """
 """
 
 # Display the HTML in Streamlit
-st.components.v1.html(html_code, height=400, scrolling=False)
+st.components.v1.html(html_code, height=430, scrolling=False)
 
