beaff9e45f3f98426bb05c0c9886e7a21fff1ee5
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-11-16 01:20:39 +0700

    Update treil.py

--- treil.py
+++ treil.py
@@ -85,6 +85,38 @@ html_code = """
             border-radius: 15px;
             box-shadow: 0 4px 20px rgba(0, 0, 0, 0.5);
         }
+        .info-box {
+            position: fixed;
+            bottom: 20px;
+            left: 20px;
+            width: 300px;
+            background-color: #f1f1f1;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+            z-index: 1000;
+        }
+        .logo {
+            width: 50px; /* Mengatur ukuran gambar */
+            height: auto; /* Menjaga rasio gambar */
+            margin-bottom: 15px;
+        }
+        .info-box h3 {
+            margin: 0;
+            font-size: 18px;
+            color: #333;
+        }
+        .info-box p {
+            margin: 5px 0;
+            color: #555;
+        }
+        .info-box a {
+            color: #007BFF;
+            text-decoration: none;
+        }
+        .info-box a:hover {
+            text-decoration: underline;
+        }
  
     </style>
 </head>
@@ -92,6 +124,23 @@ html_code = """
     <div>
         <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Logo_ITERA.png/800px-Logo_ITERA.png" alt="Logo ITE">
     </div>
+
+
+
+
+
+
+
+
+    <div class="info-box">
+        <div>
+            <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Logo_ITERA.png/800px-Logo_ITERA.png" alt="Logo ITE">
+        </div>
+    </div>
+
+
+
+    
     
     <h1>Deteksi Suara Burung di Taman Nasional Way Kambas</h1>
     
